3e6fb799a3911b2c8d966b3468d733fbc0d8f8f2
Maria <mariabrock414@gmail.com>
2019-12-11 21:17:07 -0600

    started data

---
+++ src/helpers/data/studentsData.js
@@ -0,0 +1,66 @@
+const students = [
+  {
+    id: 'student1',
+    firstName: 'Maria',
+    lastName: 'Brock',
+    isDead: false,
+  },
+  {
+    id: 'student2',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student3',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student4',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student5',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student6',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student7',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student8',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student9',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+  {
+    id: 'student10',
+    firstName: '',
+    lastName: '',
+    isDead: false,
+  },
+];
+
+const getStudents = () => students;
+
+export default { getStudents };
