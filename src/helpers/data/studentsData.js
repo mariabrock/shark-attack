d20b33235eb24d23c04fa61a9d1939737240c260
Maria <mariabrock414@gmail.com>
2019-12-12 20:02:30 -0600

    finished student data

--- src/helpers/data/studentsData.js
+++ src/helpers/data/studentsData.js
@@ -1,168 +1,206 @@
 const students = [
   {
     id: 'student1',
+    firstName: 'Alesha',
+    lastName: 'Reed',
+    isDead: false,
+  },
+  {
+    id: 'student2',
+    firstName: 'Ashley',
+    lastName: 'Claiborne',
+    isDead: false,
+  },
+  {
+    id: 'student3',
+    firstName: 'Charity',
+    lastName: 'Bunyon',
+    isDead: false,
+  },
+  {
+    id: 'student4',
+    firstName: 'Connor',
+    lastName: 'Sullivan',
+    isDead: false,
+  },
+  {
+    id: 'student5',
+    firstName: 'Crystal',
+    lastName: 'Broach',
+    isDead: false,
+  },
+  {
+    id: 'student6',
+    firstName: 'Denise',
+    lastName: 'Baker',
+    isDead: false,
+  },
+  {
+    id: 'student7',
+    firstName: 'Emilee',
+    lastName: 'Mitchell',
+    isDead: false,
+  },
+  {
+    id: 'student8',
+    firstName: 'Evan',
+    lastName: 'Grabenstein',
+    isDead: false,
+  },
+  {
+    id: 'student9',
+    firstName: 'Gabriel',
+    lastName: 'Seals',
+    isDead: false,
+  },
+  {
+    id: 'student10',
+    firstName: 'Ivan',
+    lastName: 'Phelps',
+    isDead: false,
+  },
+  {
+    id: 'student11',
+    firstName: 'Jacob',
+    lastName: 'Best-Wittenberg',
+    isDead: false,
+  },
+  {
+    id: 'student12',
+    firstName: 'Jamie',
+    lastName: 'Phillips',
+    isDead: false,
+  },
+  {
+    id: 'student13',
+    firstName: 'Jasmine',
+    lastName: 'Williams',
+    isDead: false,
+  },
+  {
+    id: 'student14',
+    firstName: 'John',
+    lastName: 'Key',
+    isDead: false,
+  },
+  {
+    id: 'student15',
+    firstName: 'John',
+    lastName: 'Thielman',
+    isDead: false,
+  },
+  {
+    id: 'student16',
+    firstName: 'John',
+    lastName: 'Johnson',
+    isDead: false,
+  },
+  {
+    id: 'student17',
+    firstName: 'Kelsey',
+    lastName: 'Creel',
+    isDead: false,
+  },
+  {
+    id: 'student18',
+    firstName: 'Laura',
+    lastName: 'Collins',
+    isDead: false,
+  },
+  {
+    id: 'student19',
+    firstName: 'Maggie',
+    lastName: 'Greene',
+    isDead: false,
+  },
+  {
+    id: 'student20',
     firstName: 'Maria',
     lastName: 'Brock',
     isDead: false,
   },
   {
-    id: 'student2',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student3',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student4',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student5',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student6',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student7',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student8',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student9',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student10',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student11',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student12',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student13',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student14',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student15',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student16',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student17',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student18',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student19',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
-    id: 'student20',
-    firstName: '',
-    lastName: '',
-    isDead: false,
-  },
-  {
     id: 'student21',
-    firstName: '',
-    lastName: '',
+    firstName: 'Monica',
+    lastName: 'Djunaidi',
     isDead: false,
   },
   {
     id: 'student22',
-    firstName: '',
-    lastName: '',
+    firstName: 'Randy',
+    lastName: 'Tate',
     isDead: false,
   },
   {
     id: 'student23',
-    firstName: '',
-    lastName: '',
+    firstName: 'Raymond',
+    lastName: 'Arceneaux',
     isDead: false,
   },
   {
     id: 'student24',
-    firstName: '',
-    lastName: '',
+    firstName: 'Callan',
+    lastName: 'Morrison',
     isDead: false,
   },
   {
     id: 'student25',
-    firstName: '',
-    lastName: '',
+    firstName: 'Greg',
+    lastName: 'Korte',
     isDead: false,
   },
   {
     id: 'student26',
-    firstName: '',
-    lastName: '',
+    firstName: 'Michael',
+    lastName: 'Clark',
     isDead: false,
   },
   {
     id: 'student27',
-    firstName: '',
-    lastName: '',
+    firstName: 'Left',
+    lastName: 'Shark',
     isDead: false,
+  },
+  {
+    id: 'student28',
+    firstName: 'Right',
+    lastName: 'Sark',
+    isDead: false,
+  },
+  {
+    id: 'student29',
+    firstName: 'Baby',
+    lastName: 'Shark',
+    isDead: false,
+  },
+  {
+    id: 'student30',
+    firstName: 'Zoe',
+    lastName: 'Ames',
+    isDead: true,
   },
 ];
 
 const getStudents = () => students;
+
+// const livingStudents = (studentId) => {
+//   students.forEach(response => {
+//     if (response.id === studentId) {
+//       response.isDead = false;
+//     }
+//   });
+//   // const studentIndex = students.find(student => student.id === studentId);
+//   // students[studentIndex ].isDead = false;
+// };
+
+// const dearlyDeparted = (studentId) => {
+//   students.forEach(response => {
+//     if (response.id === studentId) {
+//       response.isDead = true;
+//     }
+//   });
+// }
+
+// const followTheLight = () => {};
 
 export default { getStudents };
